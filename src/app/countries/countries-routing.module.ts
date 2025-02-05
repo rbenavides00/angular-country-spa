882724c6aced77833c43f094ef86e613d1be0ffe
Roberto Benavides <roberto00bl@hotmail.com>
2025-02-04 22:28:35 -0600

    Init commit

---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ByCapitalPageComponent } from './page/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './page/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './page/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './page/country-page/country-page.component';
+
+const routes: Routes = [
+  {
+    path: 'by-capital',
+    component: ByCapitalPageComponent,
+  },
+  {
+    path: 'by-country',
+    component: ByCountryPageComponent,
+  },
+  {
+    path: 'by-region',
+    component: ByRegionPageComponent,
+  },
+  {
+    path: 'by/:id',
+    component: CountryPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'by-capital',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CountriesRoutingModule {}
---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,25 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  standalone: false,
+
+  templateUrl: './search-box.component.html',
+  styles: ``,
+})
+export class SearchBoxComponent {
+  @Input()
+  public placeholder: string = '';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+}
